992ab8ec0433fc60ed350e709a995e9a0ad76ef1
tstodter <fawkes427@gmail.com>
2014-07-24 21:51:44 -0400

    Plugged in drawer events.

--- src/com/wyrdlabs/classy/DoubleDrawered.java
+++ src/com/wyrdlabs/classy/DoubleDrawered.java
@@ -5,6 +5,10 @@ import android.view.ViewGroup;
 
 public interface DoubleDrawered {
 	
+	public boolean isDrawerOpen();
+	public boolean isNavDrawerOpen();
+	public boolean isActionDrawerOpen();
+
 	public ViewGroup getNavDrawer();
 	public ViewGroup getActionDrawer();
 	public DrawerLayout getDrawerLayout();
--- src/com/wyrdlabs/classy/MainActivity.java
+++ src/com/wyrdlabs/classy/MainActivity.java
@@ -1,23 +1,24 @@
 package com.wyrdlabs.classy;
 
-import com.wyrdlabs.classy.R;
-
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- * 
- * @see SystemUiHider
- */
+
 public class MainActivity extends Activity implements DoubleDrawered {
+	
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private View mNavDrawer;
+	private View mActionDrawer;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +27,30 @@ public class MainActivity extends Activity implements DoubleDrawered {
 		Log.i("MainActivity", "in onCreate()");
 
 		setContentView(R.layout.activity_main);
+		
+		//Find and save UI components
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mNavDrawer = (View) findViewById(R.id.nav_drawer);
+		mActionDrawer = (View) findViewById(R.id.action_drawer);
+		
+		//Setup drawer
+		
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+								R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+			
+			public void onDrawerOpened(View drawerView) {
+				
+				invalidateOptionsMenu();
+			}
+			
+			public void onDrawerClosed(View drawerView) {
+				
+				invalidateOptionsMenu();
+			}
+			
+		};
+		
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
 
 		FragmentManager fm = getFragmentManager();
 		Fragment timelineFragment = fm.findFragmentByTag("timeline");
@@ -41,11 +66,28 @@ public class MainActivity extends Activity implements DoubleDrawered {
 			getActionBar().setTitle(TimelineFragment.TITLE);
 		}
 	}
-
+	
+	/* Used by drawer toggle */
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
-
+		
+		//Sync the toggle state after onRestoreInstanceState has occurred
+		mDrawerToggle.syncState();
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (mDrawerToggle.onOptionsItemSelected(item))
+			return true;
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
 	}
 	
 	public ViewGroup getActionDrawer() {
@@ -60,6 +102,21 @@ public class MainActivity extends Activity implements DoubleDrawered {
 	@Override
 	public DrawerLayout getDrawerLayout() {
 		return (DrawerLayout) findViewById(R.id.drawer_layout);
+	}
+
+	@Override
+	public boolean isDrawerOpen() {
+		return isNavDrawerOpen() || isActionDrawerOpen();
+	}
+
+	@Override
+	public boolean isNavDrawerOpen() {
+		return mDrawerLayout.isDrawerOpen(mNavDrawer);
+	}
+
+	@Override
+	public boolean isActionDrawerOpen() {
+		return mDrawerLayout.isDrawerOpen(mActionDrawer);
 	}
 
 }
