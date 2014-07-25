c333a510691088d2c9af04d056060b208985eddf
Parth Shah <prs5061@alonzo.cs.hbg.psu.edu>
2014-07-24 22:36:29 -0400

    modified:   res/menu/timeline.xml 	modified:   src/com/wyrdlabs/classy/MainActivity.java 	modified:   src/com/wyrdlabs/classy/TimelineFragment.java

--- src/com/wyrdlabs/classy/MainActivity.java
+++ src/com/wyrdlabs/classy/MainActivity.java
@@ -12,87 +12,89 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 public class MainActivity extends Activity implements DoubleDrawered {
-	
+
 	private DrawerLayout mDrawerLayout;
 	private ActionBarDrawerToggle mDrawerToggle;
 	private View mNavDrawer;
 	private View mActionDrawer;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		Log.i("MainActivity", "in onCreate()");
 
 		setContentView(R.layout.activity_main);
-		
-		//Find and save UI components
+
+		// Find and save UI components
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		mNavDrawer = (View) findViewById(R.id.nav_drawer);
 		mActionDrawer = (View) findViewById(R.id.action_drawer);
-		
-		//Setup drawer
-		
+
+		// Setup drawer
+
 		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-								R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
-			
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close) {
+
 			public void onDrawerOpened(View drawerView) {
-				
+
 				invalidateOptionsMenu();
+
 			}
-			
+
 			public void onDrawerClosed(View drawerView) {
-				
+
 				invalidateOptionsMenu();
+
 			}
-			
+
 		};
-		
+
 		mDrawerLayout.setDrawerListener(mDrawerToggle);
 
 		FragmentManager fm = getFragmentManager();
 		Fragment timelineFragment = fm.findFragmentByTag("timeline");
 		if (timelineFragment == null) {
 			Fragment fragment = new TimelineFragment();
-			
+
 			fragment.setRetainInstance(true);
-			
+
 			fm.beginTransaction()
-			  .replace(R.id.main_content_frame, fragment, "timeline")
-			  .commit();
-			
+					.replace(R.id.main_content_frame, fragment, "timeline")
+					.commit();
+
 			getActionBar().setTitle(TimelineFragment.TITLE);
 		}
 	}
-	
+
 	/* Used by drawer toggle */
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
-		
-		//Sync the toggle state after onRestoreInstanceState has occurred
+
+		// Sync the toggle state after onRestoreInstanceState has occurred
 		mDrawerToggle.syncState();
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (mDrawerToggle.onOptionsItemSelected(item))
 			return true;
-		
+
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 		mDrawerToggle.onConfigurationChanged(newConfig);
 	}
-	
+
 	public ViewGroup getActionDrawer() {
 		return (ViewGroup) findViewById(R.id.action_drawer);
-	}	
+	}
 
 	@Override
 	public ViewGroup getNavDrawer() {
--- src/com/wyrdlabs/classy/TimelineFragment.java
+++ src/com/wyrdlabs/classy/TimelineFragment.java
@@ -22,129 +22,150 @@ import android.widget.Toast;
 public class TimelineFragment extends ListFragment {
 
 	public static String TITLE = "Timeline";
-	
+
 	private Activity mActivity;
 	private View mActionDrawer;
-	
+	private DoubleDrawered mDoubleDrawer;
+
 	public TimelineFragment() {
 		setRetainInstance(true);
 		setHasOptionsMenu(true);
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		Log.i("TimelineFragment", "in onCreate()");
-	} 
-	
+	}
+
 	@Override
 	public void onPause() {
 		super.onPause();
-		
+
 		Log.i("TimelineFragment", "in onPause()");
 	}
-	
+
 	@Override
 	public void onStart() {
 		super.onStart();
-		
+
 		Log.i("TimelineFragment", "in onStart()");
 	}
-	
+
 	@Override
 	public void onStop() {
 		super.onStop();
-		
+
 		Log.i("TimelineFragment", "in onStop()");
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
-		
+
 		Log.i("TimelineFragment", "in onResume()");
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		
+
 		Log.i("TimelineFragment", "in onDestroy()");
 	}
-	
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		Log.i("TimelineFragment", "in onCreateView()");
-		
+
 		View view = inflater.inflate(R.layout.timeline, container, false);
-		
+
 		return view;
 	}
-	
+
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
 		Log.i("TimelineFragment", "in onAttach()");
-		
-		//Save activity
+
+		// Save activity
 		mActivity = activity;
-		
-		//Attach data
-		ListAdapter adapter = new ArrayAdapter<String>(activity, 
-													   R.layout.timeline_item, 
-													   R.id.event_title, 
-													   new ArrayList<String>() {{
-														   add("e1"); add("e2");
-														   add("e3"); add("e4");
-													   }});
-		
+		mDoubleDrawer = (DoubleDrawered) activity;
+
+		// Attach data
+		ListAdapter adapter = new ArrayAdapter<String>(activity,
+				R.layout.timeline_item, R.id.event_title,
+				new ArrayList<String>() {
+					{
+						add("e1");
+						add("e2");
+						add("e3");
+						add("e4");
+					}
+				});
+
 		setListAdapter(adapter);
-		
-		//Change activity UI
-		
-		//Setup action drawer
+
+		// Change activity UI
+
+		// Setup action drawer
 		LayoutInflater inflater = activity.getLayoutInflater();
-		ViewGroup actionDrawerViewGroup = ((DoubleDrawered) activity).getActionDrawer();
+		ViewGroup actionDrawerViewGroup = ((DoubleDrawered) activity)
+				.getActionDrawer();
 		actionDrawerViewGroup.removeAllViews();
-		
-		mActionDrawer = inflater.inflate(R.layout.timeline_action_drawer, actionDrawerViewGroup);
-		
+
+		mActionDrawer = inflater.inflate(R.layout.timeline_action_drawer,
+				actionDrawerViewGroup);
+
 	}
-	
+
 	@Override
 	public void onDetach() {
 		super.onDetach();
 	}
-	
-	//Add this fragment's menu items
+
+	// Add this fragment's menu items
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		Log.i("TimelineFragment", "in onCreateOptionsMenu()");
 		inflater.inflate(R.menu.timeline, menu);
 	}
-	
-	//Called right before the menu is shown, every time it is shown
-	//Use to enable/disable menu items
+
+	// Called right before the menu is shown, every time it is shown
+	// Use to enable/disable menu items
 	@Override
 	public void onPrepareOptionsMenu(Menu menu) {
 		Log.i("TimelineFragment", "in onPrepareOptionsMenu()");
-		
+		if (mDoubleDrawer.isActionDrawerOpen()) {
+			MenuItem item = menu.findItem(R.id.delete);
+			item.setVisible(true);
+			item = menu.findItem(R.id.edit);
+			item.setVisible(true);
+			item = menu.findItem(R.id.add);
+			item.setVisible(false);
+		} else {
+			MenuItem item = menu.findItem(R.id.delete);
+			item.setVisible(false);
+			item = menu.findItem(R.id.edit);
+			item.setVisible(false);
+			item = menu.findItem(R.id.add);
+			item.setVisible(true);
+		}
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Log.i("TimelineFragment", "in onOptionsItemSelected()");
-		switch(item.getItemId()) {
-			case R.id.add:		
-				Toast.makeText(mActivity, "Add clicked", Toast.LENGTH_SHORT)
-					 .show();
-				break;
-			default:
-				return false;
+		switch (item.getItemId()) {
+		case R.id.add:
+			Toast.makeText(mActivity, "Add clicked", Toast.LENGTH_SHORT).show();
+			break;
+		default:
+			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 }
